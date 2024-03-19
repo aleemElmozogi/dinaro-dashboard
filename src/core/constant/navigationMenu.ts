d15b4ba7401a51c4af3d2ece0f9752dbf7510ecb
f.shuaib <f.shuaib@masarat.ly>
2024-03-19 11:04:05 +0200

    fix ui

--- src/core/constant/navigationMenu.ts
+++ src/core/constant/navigationMenu.ts
@@ -1,5 +1,9 @@
 import { ERoutesName } from "./ERoutesName";
 
 export const navigationMenu = [
-  
+  {
+    title: "الرئيسية",
+    name: ERoutesName.DASHBOARD,
+    icon: "bx bx-home",
+  }
 ];
--- src/core/layout/SideBar.vue
+++ src/core/layout/SideBar.vue
@@ -1,6 +1,10 @@
 <template>
   <aside
-    :class="is_active ? 'translate-x-0 ' : 'ltr:-translate-x-full rtl:translate-x-full'"
+    :class="
+      is_active
+        ? 'translate-x-0 '
+        : 'ltr:-translate-x-full rtl:translate-x-full'
+    "
     class="fixed z-20 flex flex-col h-screen overflow-auto transition-transform duration-150 ease-in transform shadow-xl bg-background ltr:left-0 rtl:right-0 lg:static w-80 shadow-primary/10"
   >
     <div>
@@ -19,10 +23,10 @@
             loading="lazy"
             class="h-12 cursor-pointer"
           />
-          <h6 class="text-4xl font-medium ">Dinaro</h6>
+          <h6 class="text-4xl font-medium">Dinaro</h6>
         </div>
         <button class="lg:hidden" @click="$emit('toggleSidebar')">
-          <i class="text-3xl bx bx-menu "></i>
+          <i class="text-3xl bx bx-menu"></i>
         </button>
       </div>
     </div>
@@ -32,8 +36,8 @@
           :to="{
             name: route.name,
           }"
-          class="inline-flex flex-row items-center w-full gap-2 px-4 py-1"
-          active-class="font-medium rounded-lg bg-primary text-primary-foreground"
+          class="inline-flex flex-row items-center w-full gap-2 px-4 py-1 bg-primary-foreground"
+          active-class="font-medium rounded-lg bg-primary "
         >
           <i :class="route.icon" class="text-xl"></i>
           <span class="mx-1 leading-10 text-md !whitespace-nowrap">{{
