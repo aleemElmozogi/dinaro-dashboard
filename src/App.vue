90ea941b7e786b752b87f10e8e6c034841e8f8b3
f.shuaib <f.shuaib@masarat.ly>
2024-03-20 13:06:19 +0200

    remove theme options

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import {Theme} from "@/core/constant/Theme.ts";
-import {onMounted} from "vue";
-
-onMounted(() => {
-  if (localStorage.theme === Theme.DARK || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add(Theme.DARK)
-  } else {
-    document.documentElement.classList.remove(Theme.DARK)
-  }
-})
 
 </script>
 
